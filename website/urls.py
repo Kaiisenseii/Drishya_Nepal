ef7325d3082cdd26b99eb673646229f2804f3352
Kabir <kaiisenseii465@gmail.com>
2024-01-18 09:18:05 +0545

    Changes done here in templates and connected backend

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
+
 from . import views
 
-urlpatterns  = [
-    path('', views.home, name = 'index')
+urlpatterns = [
+    path('', views.home , name='index' ),
+    path('about', views.about, name = 'about'),
+    path('blog_details', views.blog_detail, name = 'blog_details'),
+    path('blog', views.blog, name = 'blog'),
+    path('elements', views.element, name = 'elements'),
+    path('jobs', views.job, name = 'jobs'),
+    path('contact', views.contact, name = 'contact'),
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def blog_detail(request):
+    return render(request, "blog_details.html")
+
+def blog(request):
+    return render(request, "blog.html")
+
+def element(request):
+    return render(request, "elements.html")
+
+def job(request):
+    return render(request, 'jobs.html')
+
+def contact(request):
+    return render(request, "contact.html")
